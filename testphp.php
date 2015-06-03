00fff9f87063d1daa5d185627b9ce80d83c8eb45
JennieRamos <jennieramos06@gmail.com>
2015-06-03 14:09:18 +0800

    [TASK] can create table in database

--- testphp.php
+++ testphp.php
@@ -17,14 +17,28 @@ $password = "mysql";
 $dbname = "myDB";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
-
+$conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
-echo "Connected successfully";
 
+// sql to create table
+$sql = "CREATE TABLE MyGuests (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+firstname VARCHAR(30) NOT NULL,
+lastname VARCHAR(30) NOT NULL,
+email VARCHAR(50),
+reg_date TIMESTAMP
+)";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Table MyGuests created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+
+$conn->close();
 ?>
 <br>
 <!---
@@ -40,6 +54,7 @@ if ($conn->query($sql) === TRUE) {
 $conn->close();
 ?>
 -->
+
 </body>
 </html>
 
